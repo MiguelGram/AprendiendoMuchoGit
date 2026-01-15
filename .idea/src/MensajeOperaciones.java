0aac741d359d8a8620cc1b2d072bba12c46f947a
Miguel Gramuntell <miggramar2@alu.edu.gva.es>
2026-01-15 12:26:33 +0100

    Creado MensajeOperaciones.java

---
+++ .idea/src/MensajeOperaciones.java
@@ -0,0 +1,8 @@
+public class MensajeOperaciones {
+  public void MensajeInicio(){
+    System.out.println("!Bienvenido al programa de operaciones"); 
+  }
+  public void mostrarMensaje(){
+    System.out.println("Gracias por usar el programa de operaciones"); 
+  }
+}
