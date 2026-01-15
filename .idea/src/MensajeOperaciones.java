14948bb75b749d61285732506da54c335e1a41ba
Miguel Gramuntell <miggramar2@alu.edu.gva.es>
2026-01-15 12:26:07 +0100

    Delete .idea/src/MensajeOperaciones.java

--- .idea/src/MensajeOperaciones.java
+++
@@ -1,8 +0,0 @@
-public class MensajeOperaciones {
-  public void MensajeInicio(){
-    System.out.println("!Bienvenido al programa de operaciones"); 
-  }
-  public void mostrarMensaje(){
-    System.out.println("Gracias por usar el programa de operaciones"); 
-  }
-}
